de3855b7e9442085793ea64b948e43ecbe33ab44
RobotComp.ru <RobotComp.ru@DESKTOP-94TEC7C>
2024-10-10 16:38:07 +0300

    Добавьте файлы проекта.

---
+++ Лаб 2/Program.cs
@@ -0,0 +1,17 @@
+﻿try
+{
+    Console.Write("Введите переменную e: ");
+    double e = double.Parse(Console.ReadLine());
+    const double a = 2;
+    const double p = 2.6;
+
+    double t = (Math.Pow(p, 3)) + (Math.Pow(a, 3));
+    double x = Math.Pow(e, (Math.Sqrt(p + a)));
+    double y = (Math.Pow(x, 3)) / (Math.Pow(t, 2));
+
+    Console.WriteLine($"t = {t:F2}, x = {x:F2}, y = {y:F2}");
+}
+catch
+{
+    Console.WriteLine("Введите правильно!");
+}
